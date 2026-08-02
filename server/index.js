8d35364dc23d79d87054add0f5e2a92c19356b44
Mohammed Yaseen A <yaseenyash55@gmail.com>
2026-08-02 13:19:36 +0530

    Fix Express wildcard route for SPA fallback

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // SPA fallback
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
